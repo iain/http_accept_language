d6a1eba72eb6bf8c91c66be4a364dc8509f96d15
Iain Hecker <iain@iain.nl>
2010-02-25 23:12:38 +0100

    Fix tests

--- lib/http_accept_language.rb
+++ lib/http_accept_language.rb
@@ -54,6 +54,6 @@ if defined?(ActionController::Request)
   ActionController::Request.send :include, HttpAcceptLanguage
 elsif defined?(ActionController::AbstractRequest)
   ActionController::AbstractRequest.send :include, HttpAcceptLanguage
-else
+elsif defined?(ActionController::CgiRequest)
   ActionController::CgiRequest.send :include, HttpAcceptLanguage
 end
