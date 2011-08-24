4e8eb4cb78f11b956277e587e2701c1d8a7de345
Noel Dellofano <noel@zendesk.com>
2011-08-24 12:48:48 -0700

    Add sanitization method for available locales

--- test/http_accept_language_test.rb
+++ test/http_accept_language_test.rb
@@ -37,10 +37,14 @@ class HttpAcceptLanguageTest < Test::Unit::TestCase
     request.env['HTTP_ACCEPT_LANGUAGE'] = 'en-us,de-de'
     assert_equal 'en', request.compatible_language_from(%w{de en})
   end
-  
+
   def test_should_accept_symbols_as_available_languages
     request.env['HTTP_ACCEPT_LANGUAGE'] = 'en-us'
     assert_equal :"en-HK", request.compatible_language_from([:"en-HK"])
+  end
+
+  def test_should_sanitize_available_language_names
+    assert_equal ["en-UK", "en-US", "ja-JP", "pt-BR"], request.sanitize_available_locales(%w{en_UK-x3 en-US-x1 ja_JP-x2 pt-BR-x5})
   end
 
   private
