e42d448dc7c20cceae6a61efce6223f76dc51576
grosser <grosser.michael@gmail.com>
2012-07-12 09:25:32 -0700

    support rails 2 and 3, also fail or autoload classes we depend on to prevent silent failures

--- lib/http_accept_language.rb
+++ lib/http_accept_language.rb
@@ -54,12 +54,13 @@ module HttpAcceptLanguage
       end
     end.compact.first
   end
-
 end
-if defined?(ActionDispatch::Request)
-  ActionDispatch::Request.send :include, HttpAcceptLanguage
-elsif defined?(ActionDispatch::AbstractRequest)
-  ActionDispatch::AbstractRequest.send :include, HttpAcceptLanguage
-elsif defined?(ActionDispatch::CgiRequest)
-  ActionDispatch::CgiRequest.send :include, HttpAcceptLanguage
+
+if defined?(ActionPack)
+  classes = if ActionPack::VERSION::MAJOR == 2
+    [ActionController::Request, ActionController::CgiRequest]
+  else
+    [ActionDispatch::Request]
+  end
+  classes.each{|c| c.send :include, HttpAcceptLanguage }
 end
