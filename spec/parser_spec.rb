ea0297cee2ce23e863994e07c8715774a84cf38e
Kostyantyn <kostya.stepanyuk@gmail.com>
2014-03-15 00:53:10 +0100

    Accept locales which only match RFC2616 format
    http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.4

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,7 +3,7 @@ require 'http_accept_language/parser'
 describe HttpAcceptLanguage::Parser do
 
   def parser
-    @parser ||= HttpAcceptLanguage::Parser.new('en-us,en-gb;q=0.8,en;q=0.6,es-419')
+    @parser ||= HttpAcceptLanguage::Parser.new('en-us,en-gb;q=0.8,en;q=0.6,es-ES')
   end
 
   it "should return empty array" do
@@ -12,7 +12,7 @@ describe HttpAcceptLanguage::Parser do
   end
 
   it "should properly split" do
-    parser.user_preferred_languages.should eq %w{en-US es-419 en-GB en}
+    parser.user_preferred_languages.should eq %w{en-US es-ES en-GB en}
   end
 
   it "should ignore jambled header" do
@@ -20,9 +20,14 @@ describe HttpAcceptLanguage::Parser do
     parser.user_preferred_languages.should eq []
   end
 
+  it "should ignore locales which don't match RFC2616" do
+    parser.header = 'en-US, -DE, ;q=, -, , aaaaaaaa-aaaaaaaa;q=1, bbbbbbbbb-bbbbbbbbb;q=1, it, de-DE'
+    parser.user_preferred_languages.should eq(%w[en-US aaaaaaaa-AAAAAAAA it de-DE])
+  end
+
   it "should properly respect whitespace" do
-    parser.header = 'en-us, en-gb; q=0.8,en;q = 0.6,es-419'
-    parser.user_preferred_languages.should eq %w{en-US es-419 en-GB en}
+    parser.header = 'en-us, en-gb; q=0.8,en;q = 0.6,es-ES'
+    parser.user_preferred_languages.should eq %w{en-US es-ES en-GB en}
   end
 
   it "should find first available language" do
@@ -45,7 +50,7 @@ describe HttpAcceptLanguage::Parser do
   end
 
   it "should sanitize available language names" do
-    parser.sanitize_available_locales(%w{en_UK-x3 en-US-x1 ja_JP-x2 pt-BR-x5 es-419-x4}).should eq ["en-UK", "en-US", "ja-JP", "pt-BR", "es-419"]
+    parser.sanitize_available_locales(%w{en_UK-x3 en-US-x1 ja_JP-x2 pt-BR-x5}).should eq ["en-UK", "en-US", "ja-JP", "pt-BR"]
   end
 
   it "should accept available language names as symbols and return them as strings" do
