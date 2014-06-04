d30bc0ad070a201bd2ed9b37d11a73d0985ff2b2
Matthias Hannig <hannig@trustner.com>
2014-06-04 10:52:26 +0200

    According to RFC2616 the parser should accept wildcards.
    For now it may be save to ignore them.
    However in a future implementation the wildcard should match any language
    given by the 'available_languages' array in methods determinating a preferred
    or compatible language for the user.

--- lib/http_accept_language/parser.rb
+++ lib/http_accept_language/parser.rb
@@ -18,15 +18,17 @@ module HttpAcceptLanguage
       @user_preferred_languages ||= begin
         header.to_s.gsub(/\s+/, '').split(',').map do |language|
           locale, quality = language.split(';q=')
-          raise ArgumentError, 'Not correctly formatted' unless locale =~ /^[a-z\-0-9]+$/i
+          raise ArgumentError, 'Not correctly formatted' unless locale =~ /^[a-z\-0-9]+|\*$/i
 
           locale  = locale.downcase.gsub(/-[a-z0-9]+$/i, &:upcase) # Uppercase territory
+          locale  = nil if locale == '*' # Ignore wildcards
+
           quality = quality ? quality.to_f : 1.0
 
           [locale, quality]
         end.sort do |(_, left), (_, right)|
           right <=> left
-        end.map(&:first)
+        end.map(&:first).compact
       rescue ArgumentError # Just rescue anything if the browser messed up badly.
         []
       end
